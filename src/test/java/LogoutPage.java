bdeed01d2426558eed9692effccddf586ad69196
HimaKoushal <111217320+HimaKoushal@users.noreply.github.com>
2025-06-06 17:24:23 +0530

    Update LogoutPage.java

--- src/test/java/LogoutPage.java
+++ src/test/java/LogoutPage.java
@@ -1,29 +1,24 @@
+package Practice;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
-public class LogoutPage{
+public class Logout {
 	EdgeDriver driver;
-	String url="https://www.saucedemo.com/inventory.html";
-	
-	public LogoutPage(EdgeDriver driver){
+	String URL = "https://www.saucedemo.com/inventory.html";
+	public Logout(EdgeDriver driver) {
 		this.driver=driver;
 	}
-	public void navigateToLogoutPage() {
-        if (!this.driver.getCurrentUrl().equals(this.url)) {
-            this.driver.get(this.url);
-        }
-    }
-	
-	public boolean PerformLogout() throws InterruptedException{
+	public void navigateToLogoutPage(){
+	     this.driver.get(this.URL);
+	}
+	public boolean performLogout() throws InterruptedException{
 		WebElement menu=this.driver.findElement(By.id("react-burger-menu-btn"));
 		menu.click();
 		Thread.sleep(1000);
-		WebElement logoutBtn=this.driver.findElement(By.id("logout_sidebar_link"));
-		logoutBtn.click();
-		
+		WebElement logoutbutton=this.driver.findElement(By.id("logout_sidebar_link"));
+		logoutbutton.click();
 		return true;
 	}
 }
