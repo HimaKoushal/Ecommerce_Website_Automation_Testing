308970586d54a5eb760e75c6aba2ab69d52031b3
HimaKoushal <111217320+HimaKoushal@users.noreply.github.com>
2025-06-06 17:23:00 +0530

    Update EcommerceWebsiteAutomation.java

--- src/test/java/EcommerceWebsiteAutomation.java
+++ src/test/java/EcommerceWebsiteAutomation.java
@@ -1,141 +1,97 @@
+package Practice;
 
-
-import java.net.MalformedURLException;
-
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class EcommerceWebsiteAutomation{
-	 public static void logStatus(String type, String message, String status) {
-
-	        System.out.println(String.format("%s |  %s  |  %s | %s",
-	                String.valueOf(java.time.LocalDateTime.now()), type, message, status));
-	 }
-	 
-	 public static Boolean TestCase01(EdgeDriver driver) throws InterruptedException{
-		 Boolean status;
-	     logStatus("Start TestCase", "Test Case 1: Verify Login functionality", "DONE");
-          
-	     LoginPage loginPage = new LoginPage(driver);
-	     loginPage.navigateToLoginPage();
-	     status = loginPage.PerformLogin("standard_user", "secret_sauce");
-	     if (!status) {
-	            
-	            logStatus("End TestCase", "Test Case 1: Verify Login functionality : ", status ? "PASS" : "FAIL");
-
-	            // Return False as the test case Fails
-	            return false;
-	        } else {
-	            logStatus("TestCase 1", "Test Case Pass. Login functionality ", "PASS");
-	            return true;
-	        }
-	     
-	     
-	 }
-	 
-	 public static Boolean TestCase02(EdgeDriver driver) throws InterruptedException{
-		 Boolean status;
-		 logStatus("Start TestCase", "Test Case 02: Applying filter functionality", "DONE");
-		 ApplyFilter filter=new ApplyFilter(driver);
-		 filter.navigateToHomePage();
-		 status=filter.PerformFilterAction();
-		 
-		 if (!status) {
-	            
-	            logStatus("End TestCase", "Test Case 2: Applying filter functionality : ", status ? "PASS" : "FAIL");
-
-	            // Return False as the test case Fails
-	            return false;
-	        } else {
-	            logStatus("TestCase 2", "Test Case Pass. Applying filter functionality ", "PASS");
-	            return true;
-	        }
-	 }
-	 
-	 public static Boolean TestCase03(EdgeDriver driver) throws InterruptedException{
-		 Boolean status;
-		 logStatus("Start TestCase", "Test Case 03: Add to cart functionality", "DONE");
-		 AddToCart cart= new AddToCart(driver);
-		 cart.navigateToHomePage();
-		 status=cart.PerformAddToCartFunctionality("Sauce Labs Onesie");
-		 status=cart.PerformAddToCartFunctionality("Sauce Labs Fleece Jacket");
-		 if (!status) {
-	            
-	            logStatus("End TestCase", "Test Case 03: Add to cart functionality : ", status ? "PASS" : "FAIL");
-
-	            // Return False as the test case Fails
-	            return false;
-	        } else {
-	            logStatus("TestCase 3", "Test Case Pass. Add to cart functionality ", "PASS");
-	            return true;
-	        }
-		 
-		 
-	 }
-	 
-
-	 public static Boolean TestCase04(EdgeDriver driver) throws InterruptedException{
-		 Boolean status;
-		 logStatus("Start TestCase", "Test Case 04: Checkout functionality", "DONE");
-		 CheckoutPage checkout= new CheckoutPage(driver);
-		 checkout.navigateToCartPage();
-		 status=checkout.PerformCheckoutFunctionality("Hima", "Koushal", "1234");
-		 if (!status) {
-	            
-	            logStatus("End TestCase", "Test Case 04: CheckOut functionality : ", status ? "PASS" : "FAIL");
-
-	            // Return False as the test case Fails
-	            return false;
-	        } else {
-	            logStatus("TestCase 3", "Test Case Pass.CheckOut functionality ", "PASS");
-	            return true;
-	        }
-		 
-		 
-	 }
-	 
-	 public static Boolean TestCase05(EdgeDriver driver) throws InterruptedException{
-		 Boolean status;
-	     logStatus("Start TestCase", "Test Case 05: Verify Logout functionality", "DONE");
-          
-	     LogoutPage logoutPage = new LogoutPage(driver);
-	     logoutPage.navigateToLogoutPage();
-	     status=logoutPage.PerformLogout();
-	     if (!status) {
-	            
-	            logStatus("End TestCase", "Test Case 5: Verify Logout functionality : ", status ? "PASS" : "FAIL");
-
-	            // Return False as the test case Fails
-	            return false;
-	        } else {
-	            logStatus("TestCase 5", "Test Case Pass. Logout functionality ", "PASS");
-	            return true;
-	        }
-	     
-	 }
-	 
-	  public static void main(String[] args) throws Exception {
-		  
-		  Boolean status;
-	      EdgeDriver driver = new EdgeDriver();
-	      status = TestCase01(driver);
-	      System.out.println(status);
-	      
-	      status = TestCase02(driver);
-	      System.out.println(status);
-	      
-	      status = TestCase03(driver);
-	      System.out.println(status);
-	      
-	      status = TestCase04(driver);
-	      System.out.println(status);
-	      
-	      status = TestCase05(driver);
-	      System.out.println(status);
-	      
-	      driver.quit();
-	  }
-
+	public static void logstatus(String type,String message,String status) {
+		System.out.println(String.format("%s| %s| %s| %s",String.valueOf(java.time.LocalDateTime.now()),type,message,status));
+	}
+	public static void testcase01(EdgeDriver driver) throws InterruptedException {
+		boolean status;
+		logstatus("Start Testcase 1","Verify login functionality","Done");
+		Login l = new Login(driver);
+		l.navigateToLoginPage();
+		Thread.sleep(1000);
+		status=l.performLogin("standard_user","secret_sauce");
+		Thread.sleep(1000);
+		if(!status) {
+			logstatus("End testcase","Testcase 1 : Verify login functionality","Fail");
+		}
+		else {
+			System.out.println("Testcase 1 : Login functionality-Pass");
+		}	
+	}
 	
-	
+	public static void testcase02(EdgeDriver driver) throws InterruptedException {
+		boolean status1;
+		logstatus("Start Testcase 2","Verify apply filter","Done");
+		ApplyFilter a = new ApplyFilter(driver);
+		a.navigateToHomePage();
+		Thread.sleep(1000);
+		status1=a.performApplyFilter();
+		Thread.sleep(1000);
+		if(!status1) {
+			logstatus("End testcase","Testcase 2 : Verify apply filter","Fail");
+		}
+		else {
+			System.out.println("Testcase 2 : Apply filter-Pass");
+		}	
+	}
+	public static void testcase03(EdgeDriver driver) throws InterruptedException {
+		boolean status2;
+		logstatus("Start Testcase 3","Verify add to cart","Done");
+		AddToCart c = new AddToCart(driver);
+		status2=c.performAddToCart("Sauce Labs Backpack");
+		Thread.sleep(1000);
+		if(!status2) {
+			logstatus("End testcase","Testcase 3 : Verify add to cart","Fail");
+		}
+		else {
+			System.out.println("Testcase 3 : Add to cart-Pass");
+		}
+	}
+	public static void testcase04(EdgeDriver driver) throws InterruptedException {
+		boolean status3;
+		logstatus("Start Testcase 4","Verify checkout","Done");
+		CheckoutPage t = new CheckoutPage(driver);
+		t.navigateToCart();
+		Thread.sleep(1000);
+		status3=t.performCheckout("Hima", "Koushal", "1234");
+		Thread.sleep(1000);
+		if(!status3) {
+			logstatus("End testcase","Testcase 4 : Verify checkout","Fail");
+		}
+		else {
+			System.out.println("Testcase 4 : Checkout-Pass");
+		}
+	}
+	public static void testcase05(EdgeDriver driver) throws InterruptedException {
+		boolean status4;
+		logstatus("Start testcase 5","Verify logout","Done");
+		Logout lt = new Logout(driver);
+		lt.navigateToLogoutPage();
+		Thread.sleep(1000);
+		status4=lt.performLogout();
+		Thread.sleep(1000);
+		if(!status4) {
+			logstatus("End testcase","Testcase 5 : Verify logout","Fail");
+		}
+		else {
+			System.out.println("Testcase 5 : Logout-Pass");
+		}
+	}
+	public static void main(String[] args) throws InterruptedException {
+		EdgeDriver driver = new EdgeDriver();
+		testcase01(driver);
+		Thread.sleep(3000);
+		testcase02(driver);
+		Thread.sleep(2000);
+		testcase03(driver);
+		Thread.sleep(2000);
+		testcase04(driver);
+		Thread.sleep(2000);
+		testcase05(driver);
+		Thread.sleep(2000);
+		driver.quit();
+	}
 }
