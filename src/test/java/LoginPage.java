bb2231db3eea1bc540be52e5279d404dc033c438
HimaKoushal <111217320+HimaKoushal@users.noreply.github.com>
2025-06-06 17:23:47 +0530

    Update LoginPage.java

--- src/test/java/LoginPage.java
+++ src/test/java/LoginPage.java
@@ -1,33 +1,28 @@
-
+package Practice;
+import java.sql.Driver;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
-public class LoginPage{
-	EdgeDriver driver;
-	String url="https://www.saucedemo.com/";
+public class Login{
+EdgeDriver driver;
+String URL="https://saucedemo.com";
+public Login(EdgeDriver driver) {
+	this.driver=driver;
+}
+public void navigateToLoginPage() {
+	this.driver.get(this.URL);
+}
+public boolean performLogin(String Username,String Password){
+	WebElement userName=this.driver.findElement(By.id("user-name"));
+	userName.sendKeys(Username);
 	
-	public LoginPage(EdgeDriver driver){
-		this.driver=driver;
-	}
-	public void navigateToLoginPage() {
-        if (!this.driver.getCurrentUrl().equals(this.url)) {
-            this.driver.get(this.url);
-        }
-    }
+	WebElement passWord=this.driver.findElement(By.id("password"));
+	passWord.sendKeys(Password);
 	
-	public boolean PerformLogin(String Username ,String Password) throws InterruptedException{
-		WebElement userName=this.driver.findElement(By.id("user-name"));
-		userName.sendKeys(Username);
-		
-		WebElement passWord=this.driver.findElement(By.id("password"));
-		passWord.sendKeys(Password);
-		
-		WebElement loginBtn=this.driver.findElement(By.id("login-button"));
-		loginBtn.click();
-		return true;
-	}
+	WebElement loginBtn=this.driver.findElement(By.id("login-button"));
+	loginBtn.click();
+	return true;
+}
 }
